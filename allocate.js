0a143d1395dad7299fdc9826ec6a4399671cab0c
icebergdelphi <iceberg350@hotmail.com>
2023-08-01 14:39:02 -0500

    Code Changes in Allocate

--- allocate.js
+++ allocate.js
@@ -1,42 +1,35 @@
 module.exports = function allocate(salesOrders, purchaseOrders) {
-    //first  Sort both arrays based on dates
-    salesOrders.sort((a, b) => new Date(a.created) - new Date(b.created));
-    purchaseOrders.sort((a, b) => new Date(a.receiving) - new Date(b.receiving));
-  
-    const result = [];
-    let demandPointer = 0;
-    let supplyPointer = 0;
-  
-    while (demandPointer < salesOrders.length && supplyPointer < purchaseOrders.length) {
-      const demandOrder = salesOrders[demandPointer];
-      const supplyOrder = purchaseOrders[supplyPointer];
-  
-      if (supplyOrder.quantity >= demandOrder.quantity) {
-        // Sufficient supply to fulfill demand
-        result.push({
-          id: demandOrder.id,
-          expectedDelivery: supplyOrder.receiving,
-        });
-  
-        // Reduce the available supply
-        supplyOrder.quantity -= demandOrder.quantity;
-  
-        // Move to the next sales order
-        demandPointer++;
-      } else {
-        // Insufficient supply, allocate what is available
-        result.push({
-          id: demandOrder.id,
-          expectedDelivery: supplyOrder.receiving,
-        });
-  
-        // Update the remaining demand
-        demandOrder.quantity -= supplyOrder.quantity;
-  
-        // Move to the next supply order
-        supplyPointer++;
-      }
+ //first Sort both arrays based on dates
+  salesOrders.sort((a, b) => new Date(a.created) - new Date(b.created));
+  purchaseOrders.sort((a, b) => new Date(a.receiving) - new Date(b.receiving));
+
+  let stock = 0;
+  let purchaseIndex = 0;
+  const result = [];
+
+  for (const sale of salesOrders) {
+    while (purchaseIndex < purchaseOrders.length && stock < sale.quantity) {
+      const purchase = purchaseOrders[purchaseIndex];
+      stock += purchase.quantity;
+      purchaseIndex++;
     }
-  
-    return result;
+
+    if (stock >= sale.quantity) {
+      // The sale can be fulfilled
+      const purchase = purchaseOrders[purchaseIndex - 1];
+      result.push({
+        id: sale.id,
+        availabilityDate: purchase.receiving,
+      });
+      stock -= sale.quantity;
+    } else {
+      // Insufficient stock to fulfill the sale
+      result.push({
+        id: sale.id,
+        availabilityDate: "Insufficient stock to fulfill the sale, need more purchases",
+      });
+    }
   }
+
+  return result;
+}
